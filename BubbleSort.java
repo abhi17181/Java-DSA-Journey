eaee74628e3bdaa068d017fcc640657cb111f1ec
Abhishek Kumar Singh <150012887+abhi17181@users.noreply.github.com>
2025-06-24 12:08:07 +0530

    Add files via upload
    Performed Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    public static void bubbleSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int k=0;k<arr.length;k++){
+            System.out.print(arr[k]+"");
+        }
+        System.out.println();
+    }
+    public static void main(String[]args){
+        int arr[]={3,5,1,7,4};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
