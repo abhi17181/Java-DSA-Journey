a43e085ea4da83084191abfad2c84d3f0ff0faf9
Abhishek Kumar Singh <150012887+abhi17181@users.noreply.github.com>
2025-07-02 15:35:51 +0530

    Create RemoveFromLast.java
    feat: implement delete Nth from End to remove the nth node from end of LinkedList

---
+++ RemoveFromLast.java
@@ -0,0 +1,74 @@
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+    public void print(){
+        if(head==null){
+            System.out.println("LinkedList is Empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public void deleteNthfromEnd(int n){
+        int sz=0;
+        Node temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            sz++;
+        }
+        if(n==sz){
+            head=head.next;
+            return;
+        }
+        int i=1;
+        int idxToFind=sz-n;
+        Node prev=head;
+        while(i<idxToFind){
+            prev=prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+        return;
+    }
+    public static void main(String[]args){
+        LinkedList ll=new LinkedList();
+        
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.print();
+        ll.deleteNthfromEnd(3);
+        ll.print();
+    }
+}
