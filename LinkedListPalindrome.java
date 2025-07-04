e2eab86b7c6e262abd2fd450199e2423cfb95d8d
Abhishek Kumar Singh <150012887+abhi17181@users.noreply.github.com>
2025-07-04 17:32:01 +0530

    Create LinkedListPalindrome.java
    Implement LinkedList with palindrome check using mid-point and second-half reversal
    Also used Slow-Fast Approach

---
+++ LinkedListPalindrome.java
@@ -0,0 +1,85 @@
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+    public void print(){
+        if(head==null){
+            System.out.println("LinkedList is Empty");
+            return;
+        }
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public Node findMid(Node head){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public boolean checkPalindrome(){
+        if(head==null || head.next==null){
+            return true;
+        }
+        Node midNode=findMid(head);
+        Node prev=null;
+        Node curr=midNode;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        Node right=prev;
+        Node left=head;
+        while(right!=null){
+            if(left.data!=right.data){
+                return false;
+            }
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+    }
+    public static void main(String[]args){
+        LinkedList ll=new LinkedList();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.print();
+        System.out.println(ll.checkPalindrome());
+    }
+}
