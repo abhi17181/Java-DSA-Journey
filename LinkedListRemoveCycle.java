53c84ea3f5d2af8a8e211e322ae337450d7737c0
Abhishek Kumar Singh <150012887+abhi17181@users.noreply.github.com>
2025-07-05 15:03:55 +0530

    Create LinkedListRemoveCycle.java
    feat: added logic to detect and remove cycle in LinkedList using Floyd’s algorithm

---
+++ LinkedListRemoveCycle.java
@@ -0,0 +1,60 @@
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static boolean checkLoop(){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void removeCycle(){
+        Node slow=head;
+        Node fast=head;
+        boolean cycle=false;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                cycle=true;
+                break;
+            }
+        }
+        if(cycle==false){
+            return;
+        }
+        slow=head;
+        Node prev=null;
+        while(slow!=fast){
+            prev=fast;
+            slow=slow.next;
+            fast=fast.next;
+        }
+        prev.next=null;
+    }
+    public static void main(String[]args){
+        LinkedList ll=new LinkedList();
+        head=new Node(1);
+        Node temp=new Node(2);
+        head.next=temp;
+        head.next.next=new Node(3);
+        head.next.next.next=temp;
+        System.out.println(checkLoop());
+        removeCycle();
+        System.out.println(checkLoop());
+    
+    }
+}
