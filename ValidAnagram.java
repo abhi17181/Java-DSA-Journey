2abb8141b1004a4ed48878d4e6fd3c34565cdb4b
Abhishek Kumar Singh <150012887+abhi17181@users.noreply.github.com>
2025-08-20 13:09:24 +0530

    Update ValidAnagram.java
    Added program to check if two strings are valid anagrams using HashMap

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class ValidAnagram {
     public static boolean isAnagram(String s,String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
         HashMap<Character,Integer> map=new HashMap<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
@@ -27,3 +30,4 @@ public class ValidAnagram {
         System.out.println(isAnagram(s, t));
     }
 }
+
