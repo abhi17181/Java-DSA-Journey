0309e943968cad917f873e146d07a0b5450fb19c
Abhishek Kumar Singh <150012887+abhi17181@users.noreply.github.com>
2025-08-21 14:50:38 +0530

    Create LinkedHashSet.java
    Add example of LinkedHashSet to demonstrate insertion order preservation

---
+++ LinkedHashSet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Iteration{
+    public static void main(String[]args){
+        LinkedHashSet<String> lhs=new  LinkedHashSet<>();
+        lhs.add("delhi");
+        lhs.add("mumbai");
+        lhs.add("Noida");
+        lhs.add("up");
+        System.out.println(lhs);
+    }
+}
