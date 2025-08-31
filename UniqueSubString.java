ad81fd4c1de13236ce8d5df5dcd3b51971568eea
Abhishek Kumar Singh <150012887+abhi17181@users.noreply.github.com>
2025-08-31 23:24:56 +0530

    Add files via upload
    Implemented unique substring counter using Trie by inserting all suffixes and counting total nodes.

---
+++ UniqueSubString.java
@@ -0,0 +1,56 @@
+package trie;
+
+public class UniqueSubString {
+    static class Node{
+        Node children[]=new Node[26];
+        boolean endOfWord;
+        public Node(){
+            for(int i=0;i<26;i++){
+                children[i]=null;
+            }
+        }
+    }
+    public static Node root=new Node();
+    public static void insert(String word){
+        Node curr=root;
+        for(int level=0;level<word.length();level++){
+            int idx=word.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                curr.children[idx]=new Node();
+            }
+            curr=curr.children[idx];
+        }
+        curr.endOfWord=true;
+    }
+    public static boolean search(String key){
+        Node curr=root;
+        for(int level=0;level<key.length();level++){
+            int idx=key.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                return false;
+            }
+            curr=curr.children[idx];
+        }
+        return curr.endOfWord==true;
+    }
+    public static int countNodes(Node root){
+        if(root==null){
+            return 0;
+        }
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(root.children[i]!=null){
+                count+=countNodes(root.children[i]);
+            }
+        }
+        return count+1;
+    }
+    public static void main(String[]agrs){
+        String str="ababa";
+        for (int i=0;i<str.length();i++){
+            String suffix=str.substring(i);
+            insert(suffix);
+        }
+        System.out.println(countNodes(root));
+    }
+}
